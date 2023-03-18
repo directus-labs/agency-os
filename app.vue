a21795501d07219c2cafc3a3de7f709adc9abfa5
bryantgillespie <hey@bryantgillespie.com>
2023-03-17 21:19:47 -0400

    app.vue

--- app.vue
+++ app.vue
@@ -1,13 +1,3 @@
-<script lang="ts">
-// useServerSeoMeta({
-//   title: 'My Amazing Site',
-//   ogTitle: 'My Amazing Site',
-//   description: 'This is my amazing site, let me tell you all about it.',
-//   ogDescription: 'This is my amazing site, let me tell you all about it.',
-//   ogImage: 'https://example.com/image.png',
-//   twitterCard: 'summary_large_image',
-// })
-</script>
 <template>
   <NuxtLayout>
     <NuxtLoadingIndicator
