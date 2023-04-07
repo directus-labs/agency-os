86bfb954e17fe1c03b47edaad90f8cedb61de4c9
bryantgillespie <hey@bryantgillespie.com>
2023-04-07 11:31:16 -0400

    bump deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -66,7 +66,6 @@ module.exports = {
   },
   plugins: [
     require('@tailwindcss/forms'),
-    require('@tailwindcss/line-clamp'),
     require('@tailwindcss/aspect-ratio'),
     // Formkit Plugin for Tailwind
     // https://formkit.com/guides/create-a-tailwind-theme
