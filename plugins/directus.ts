3d82f64b8c401a0427d71299515e56d774ea8423
bryantgillespie <hey@bryantgillespie.com>
2023-07-03 18:25:01 -0400

    typings for Directus plugin

--- plugins/directus.ts
+++ plugins/directus.ts
@@ -1,4 +1,5 @@
 import { Directus } from '@directus/sdk'
+import { DirectusCollections } from 'types'
 // Make sure you review the Directus SDK documentation for more information
 // https://docs.directus.io/reference/sdk.html
 
@@ -6,11 +7,14 @@ export default defineNuxtPlugin((nuxtApp) => {
   const config = useRuntimeConfig()
 
   // Create a new instance of the SDK
-  const directus = new Directus(config.public.directusUrl, {
-    // auth: {
-    //   staticToken: config.directusToken,
-    // },
-  })
+  const directus = new Directus<DirectusCollections>(
+    config.public.directusUrl,
+    {
+      // auth: {
+      //   staticToken: config.directusToken,
+      // },
+    }
+  )
 
   // Inject the SDK into the Nuxt app
   // Can be accessed from anywhere in the app using $directus
