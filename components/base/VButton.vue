16fb4d9d0512493a84d82bf454246563a262194b
bryantgillespie <hey@bryantgillespie.com>
2023-07-03 17:59:26 -0400

    new button sizes

--- components/base/VButton.vue
+++ components/base/VButton.vue
@@ -1,6 +1,37 @@
+<script setup lang="ts">
+import { NuxtLink } from '#components'
+
+interface ButtonProps {
+  type?: 'button' | 'submit' | 'reset'
+  variant?: 'default' | 'primary' | 'outline' | 'danger'
+  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl'
+  loading?: boolean
+  disabled?: boolean
+  block?: boolean
+  target?: string
+  href?: string
+}
+
+const props = withDefaults(defineProps<ButtonProps>(), {
+  type: 'button',
+  variant: 'default',
+  size: 'md',
+})
+
+const as = computed(() => {
+  if (props.href) return NuxtLink
+  else return 'button'
+})
+
+const buttonProps = computed(() => {
+  if (props.href) return { to: props.href, target: props.target }
+  else return { disabled: props.disabled, type: props.type }
+})
+</script>
 <template>
-  <button
-    :type="type"
+  <component
+    :is="as"
+    v-bind="buttonProps"
     :class="[
       'btn',
       {
@@ -9,21 +40,15 @@
         'btn-outline': variant === 'outline',
         'btn-danger': variant === 'danger',
       },
+      {
+        'btn-xs': size === 'xs',
+        'btn-sm': size === 'sm',
+        'btn-md': size === 'md',
+        'btn-lg': size === 'lg',
+        'btn-xl': size === 'xl',
+      },
     ]"
   >
     <slot />
-  </button>
+  </component>
 </template>
-
-<script setup>
-const props = defineProps({
-  type: {
-    type: String,
-    default: 'button',
-  },
-  variant: {
-    type: String,
-    default: 'default',
-  },
-})
-</script>
