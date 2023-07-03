427ab7fd327deda32912052f18a0dbb176d325a5
bryantgillespie <hey@bryantgillespie.com>
2023-07-03 18:00:26 -0400

    string utils to ts

--- utils/strings.ts
+++ utils/strings.ts
@@ -1,15 +1,18 @@
 import { v4 as uuidv4 } from 'uuid'
 
-function generateId() {
+function generateId(): string {
   return uuidv4()
 }
 
-function stripHTML(original) {
+function stripHTML(original: string | undefined): string | undefined {
   if (!original) return
   else return original.replace(/(<([^>]+)>)/gi, '')
 }
 
-function truncateString(str, num) {
+function truncateString(
+  str: string | undefined,
+  num: number
+): string | undefined {
   if (!str) return
   if (str.length <= num) {
     return str
@@ -17,13 +20,16 @@ function truncateString(str, num) {
   return str.slice(0, num) + '...'
 }
 
-function truncateHTML(html, num) {
+function truncateHTML(
+  html: string | undefined,
+  num: number
+): string | undefined {
   if (!html) return
   return truncateString(stripHTML(html), num)
 }
 
 // Slugify a string for hyphens and underscores
-function slugify(str) {
+function slugify(str: string | undefined): string | undefined {
   if (!str) return
   return str
     .toString()
@@ -34,7 +40,7 @@ function slugify(str) {
 }
 
 // Deslugify a string for hyphens and underscores and capitalize each word
-function deslugify(str) {
+function deslugify(str: string | undefined): string | undefined {
   if (!str) return
   return str
     .trim()
@@ -44,24 +50,49 @@ function deslugify(str) {
     .replace(/(^| )(\w)/g, (s) => s.toUpperCase())
 }
 
-function getDomainNameFromEmail(email) {
+function getDomainNameFromEmail(email: string): string {
   const temp = email.replace(/.*@/, '').split('.')
   return temp[temp.length - 2]
 }
 
-function toTitleCase(str) {
+function toTitleCase(str: string | undefined): string | undefined {
   if (!str) return
   return str.replace(/\w\S*/g, function (txt) {
     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
   })
 }
 
-function maybePluralize(count, noun, suffix = 's') {
+function snakeToCamel(s: string): string {
+  return s.replace(/(_\w)/g, function (m) {
+    return m[1].toUpperCase()
+  })
+}
+
+function convertIconName(name: string) {
+  // Convert the icon coming from the API to the name of the icon component
+  // Directus uses Google Material Icons and the icon values are snake_case (e.g. "account_circle")
+  const prefix = 'material-symbols:'
+  // Change snake case to kebab case
+  const kebabCase = name.replace(/_/g, '-')
+  const iconName = prefix + kebabCase
+  return iconName
+}
+
+function snakeToKebab(s: string): string {
+  return s.replace(/(_\w)/g, function (m) {
+    return '-' + m[1].toLowerCase()
+  })
+}
+
+function maybePluralize(count: number, noun: string, suffix = 's'): string {
   return `${noun}${count !== 1 ? suffix : ''}`
 }
 
 // Calculate read time for a string
-function calculateReadTime(str, wordsPerMinute = 200) {
+function calculateReadTime(
+  str: string | undefined,
+  wordsPerMinute = 200
+): string | undefined {
   if (!str) return
   // Strip HTML tags
   str = str.replace(/(<([^>]+)>)/gi, '')
@@ -78,6 +109,9 @@ export {
   truncateString,
   maybePluralize,
   toTitleCase,
+  snakeToCamel,
+  snakeToKebab,
+  convertIconName,
   slugify,
   deslugify,
   getDomainNameFromEmail,
