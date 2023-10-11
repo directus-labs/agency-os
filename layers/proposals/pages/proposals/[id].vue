da20599d5b50f116bd9b8fcc79b70cbe752515ee
bryantgillespie <hey@bryantgillespie.com>
2023-10-10 21:17:46 -0400

    proposals wip layer

---
+++ layers/proposals/pages/proposals/[id].vue
@@ -0,0 +1,128 @@
+<script setup lang="ts">
+definePageMeta({
+	layout: 'proposal',
+});
+const { $directus, $readItem } = useNuxtApp();
+const { params, path } = useRoute();
+
+const { showSidebar, toggleSidebar } = useProposals();
+
+// Fetch the page data from the Directus API using the Nuxt useAsyncData composable
+// https://v3.nuxtjs.org/docs/usage/data-fetching#useasyncdata
+const {
+	data: proposal,
+	pending,
+	error,
+} = await useAsyncData(path, () => {
+	return $directus.request(
+		$readItem('os_proposals', params.id, {
+			fields: [
+				'name',
+				{
+					organization: ['name', 'logo', 'brand_color'],
+					owner: ['first_name', 'last_name', 'avatar', 'title'],
+
+					// contacts: { contact_id: ['first_name', 'last_name'] }
+					blocks: [
+						'collection',
+						{
+							item: {
+								block_hero: ['id', 'title', 'headline', 'content', 'image', 'buttons', 'image_position'],
+								block_faqs: ['id', 'title', 'faqs', 'headline', 'alignment'],
+								block_richtext: ['id', 'title', 'headline', 'content', 'alignment'],
+								block_testimonials: [
+									'id',
+									'title',
+									'headline',
+									{
+										testimonials: [
+											{
+												testimonials_id: [
+													'id',
+													'title',
+													'subtitle',
+													'content',
+													'company',
+													'company_logo',
+													{ image: ['id', 'title', 'description'] },
+												],
+											},
+										],
+									},
+								],
+								block_quote: ['id', 'title', 'subtitle', 'content'],
+								block_cta: ['id', 'title', 'headline', 'content', 'buttons'],
+								block_form: ['id', 'title', 'headline', { form: ['*'] }],
+								block_logocloud: ['id', 'title', 'headline', { logos: ['*'] }],
+								block_gallery: [
+									'id',
+									'title',
+									'headline',
+									{
+										gallery_items: [
+											{
+												directus_files_id: ['id', 'title', 'description'],
+											},
+										],
+									},
+								],
+								block_steps: [
+									'id',
+									'title',
+									'headline',
+									'show_step_numbers',
+									'alternate_image_position',
+									{
+										steps: ['id', 'title', 'content', 'image'],
+									},
+								],
+								block_columns: [
+									'id',
+									'title',
+									'headline',
+									{
+										rows: ['title', 'headline', 'content', 'image_position', { image: ['id', 'title', 'description'] }],
+									},
+								],
+								block_divider: ['id', 'title'],
+								block_team: ['*'],
+								block_html: ['*'],
+								block_video: ['*'],
+								block_cardgroup: ['*'],
+							},
+						},
+					],
+				},
+			],
+		}),
+	);
+});
+
+if (!proposal.value) {
+	// console.log(error.value.cause);
+	throw createError({ statusCode: 404, statusMessage: 'Page Not Found' });
+}
+</script>
+<template>
+	<div class="">
+		<ProposalsHeader
+			:title="proposal?.name"
+			class="fixed top-0 z-20 flex justify-center w-full p-2"
+			:name="proposal?.name"
+			:organization="proposal?.organization?.name"
+		/>
+		<div class="overflow-y-auto">
+			<ProposalsBlocksHero
+				class="pt-36"
+				:name="proposal?.name"
+				:owner="proposal.owner"
+				:organization="proposal?.organization?.name"
+			/>
+			<!-- Render the page using the PageBuilder component -->
+
+			<PageBuilder :page="proposal" id="content" />
+
+			<ProposalsBlocksAcceptance id="accept" class="max-w-3xl mx-auto" />
+		</div>
+	</div>
+</template>
