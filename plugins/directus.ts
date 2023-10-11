b79c6060a54df0d61dda8abc7b762b904febdf06
bryantgillespie <hey@bryantgillespie.com>
2023-10-10 22:52:31 -0400

    cleanup plugin

--- plugins/directus.ts
+++ plugins/directus.ts
@@ -13,7 +13,6 @@ import {
 	readFolder,
 	readFolders,
 	rest,
-	realtime,
 	staticToken,
 } from '@directus/sdk';
 import type { Schema } from '~/types/schema';
@@ -21,22 +20,12 @@ import type { Schema } from '~/types/schema';
 export default defineNuxtPlugin(() => {
 	const config = useRuntimeConfig();
 	const directusUrl = config.public.directusUrl as string;
-	const directusWsUrl = config.public.directusWsUrl as string;
 
 	const directus = createDirectus<Schema>(directusUrl).with(rest());
-
-	const ws = createDirectus<Schema>(directusWsUrl)
-		.with(staticToken(config.public.directusToken as string))
-		.with(
-			realtime({
-				authMode: 'public',
-			}),
-		);
 
 	return {
 		provide: {
 			directus,
-			ws,
 			readItem,
 			createItem,
 			updateItem,
