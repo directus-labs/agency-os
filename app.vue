5ef2a08a1a6377a297b2de6470f58aa78fb9280a
bryantgillespie <hey@bryantgillespie.com>
2023-10-21 09:42:18 -0400

    global theme + jsonld

--- app.vue
+++ app.vue
@@ -1,3 +1,34 @@
+<script setup lang="ts">
+import { borderRadiusMap } from '~/theme';
+
+const { globals, theme } = useAppConfig();
+const { fileUrl } = useFiles();
+
+// JSON-LD
+useSchemaOrg([
+	defineOrganization({
+		name: globals?.title ?? 'AgencyOS',
+		logo: globals?.logo_on_light_bg ? fileUrl(globals?.logo_on_light_bg) : '/logos/agencyos.png',
+		sameAs: () => {
+			const socialLinks = globals?.social_links ?? [];
+			return socialLinks.map((link) => link.url);
+		},
+	}),
+]);
+
+useHead({
+	style: [
+		{
+			id: 'border-radius',
+			innerHTML: `:root {${Object.entries(borderRadiusMap[theme.borderRadius])
+				.map(([key, value]) => `--border-radius-${key}: ${value};`)
+				.join('\n')}\n${Object.entries(theme.fonts)
+				.map(([key, value]) => `--font-${key}: ${value};`)
+				.join('\n')}`,
+		},
+	],
+});
+</script>
 <template>
 	<NuxtLayout>
 		<NuxtLoadingIndicator
