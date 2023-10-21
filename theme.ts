d25786b2ab2fd1d059954d41c90234ed32e8ae38
bryantgillespie <hey@bryantgillespie.com>
2023-10-21 09:42:38 -0400

    theming support

--- theme.ts
+++ theme.ts
@@ -1,22 +1,22 @@
 interface Theme {
 	primary: string;
 	gray: string;
-	borderRadius: 'none' | 'sm' | 'base' | 'md' | 'lg' | 'xl' | '2xl' | '3xl' | 'full';
+	borderRadius: 'none' | 'sm' | 'base' | 'md' | 'lg' | 'xl' | 'full';
 	googleFonts: {
 		[key: string]: boolean | number[];
 	};
 	fonts: {
 		display: string;
-		body: string;
+		sans: string;
 		code: string;
 		signature: string;
 	};
 }
 
 export const theme = {
-	primary: 'violet',
-	gray: 'slate',
-	borderRadius: 'xl',
+	primary: 'lime',
+	gray: 'zinc',
+	borderRadius: 'none',
 	googleFonts: {
 		Inter: true,
 		'Fira Code': true,
@@ -25,8 +25,54 @@ export const theme = {
 	},
 	fonts: {
 		display: 'Poppins',
-		body: 'Inter',
+		sans: 'Inter',
 		code: 'Fira Code',
 		signature: 'Nothing You Could Do',
 	},
 } as Theme;
+
+export const borderRadiusMap = {
+	none: {
+		card: '0px',
+		button: '0px',
+		input: '0px',
+		panel: '0px',
+	},
+	sm: {
+		card: '0.125rem',
+		button: '0.125rem',
+		input: '0.125rem',
+		panel: '0.125rem',
+	},
+	base: {
+		card: '0.25rem',
+		button: '0.25rem',
+		input: '0.25rem',
+		panel: '0.25rem',
+	},
+
+	md: {
+		card: '0.375rem',
+		button: '0.375rem',
+		input: '0.375rem',
+		panel: '0.375rem',
+	},
+	lg: {
+		card: '0.5rem',
+		button: '0.5rem',
+		input: '0.5rem',
+		panel: '0.5rem',
+	},
+	xl: {
+		card: '0.75rem',
+		button: '0.75rem',
+		input: '0.5rem',
+		panel: '0.75rem',
+	},
+	full: {
+		card: '0.75rem',
+		button: '9999px',
+		input: '9999px',
+		panel: '0.75rem',
+	},
+};
