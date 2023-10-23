e14391eda3849e43b5891fb302e474172ae3ec10
bryantgillespie <hey@bryantgillespie.com>
2023-10-23 10:27:07 -0400

    directus server util

--- server/utils/directus-server.ts
+++ server/utils/directus-server.ts
@@ -1,14 +1,11 @@
 import {
-	aggregate,
 	createDirectus,
 	readItem,
 	readItems,
 	readSingleton,
 	rest,
 	createItem,
-	createItems,
 	updateItem,
-	updateItems,
 	staticToken,
 	withToken,
 } from '@directus/sdk';
@@ -20,15 +17,4 @@ const directusServer = createDirectus<Schema>(directusUrl)
 	.with(rest())
 	.with(staticToken(process.env.DIRECTUS_ADMIN_TOKEN as string));
 
-export {
-	directusServer,
-	readItem,
-	readItems,
-	readSingleton,
-	aggregate,
-	createItem,
-	createItems,
-	updateItem,
-	updateItems,
-	withToken,
-};
+export { directusServer, readItem, readItems, readSingleton, createItem, updateItem, withToken };
