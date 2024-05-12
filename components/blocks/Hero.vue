61f7bfdf53466d6ac956cedf2c946af503e9d7ef
Nathan Doyle <natetronn@gmail.com>
2024-05-12 00:16:24 -0700

    Hero block translations (WIP)

--- components/blocks/Hero.vue
+++ components/blocks/Hero.vue
@@ -1,33 +1,52 @@
 <script setup lang="ts">
-import type { BlockHero, BlockButtonGroup } from '~/types';
+import type { BlockHero, BlockButtonGroup } from "~/types";
+
+const { t } = useI18n();
 
 defineProps<{
-	data: BlockHero;
+  data: BlockHero;
 }>();
 </script>
 <template>
-	<BlockContainer class="relative grid gap-12 md:grid-cols-3">
-		<!-- Content -->
-		<div class="md:pt-12 md:col-span-2">
-			<TypographyTitle v-if="data.title">
+  <BlockContainer class="relative grid gap-12 md:grid-cols-3">
+    <!-- <h1 style="color: white">{{ data }}</h1> -->
+
+    <!-- Content -->
+    <div class="md:pt-12 md:col-span-2">
+      <!-- <TypographyTitle v-if="data.title">
 				{{ data.title }}
-			</TypographyTitle>
-			<TypographyHeadline v-if="data.headline" :content="data.headline" size="title" as="h1" />
-			<TypographyProse v-if="data.content" :content="data.content" size="lg" class="py-6 font-display" />
-			<BlocksButtonGroup v-if="data.button_group" :data="data.button_group as BlockButtonGroup" />
-		</div>
-		<!-- Image -->
-		<div
-			v-if="data.image"
-			class="overflow-hidden border lg:relative lg:h-full dark:border-gray-700 rounded-card"
-			:class="data.image_position === 'left' ? 'order-first lg:-ml-48 md:-ml-16' : 'lg:-mr-48 md:-mr-16 '"
-		>
-			<NuxtImg
-				v-if="data.image"
-				class="w-full overflow-hidden dark:brightness-90 max-h-[700px] h-full object-cover rounded-card"
-				:src="data.image as string"
-				alt=""
-			/>
-		</div>
-	</BlockContainer>
+			</TypographyTitle> -->
+      <TypographyTitle v-if="t('data.translations[0].title')">
+        {{ t("data.translations[0].title") }}
+      </TypographyTitle>
+      <TypographyHeadline
+        v-if="t('data.translations[0].headline')"
+        :content="t('data.translations[0].headline')"
+        size="title"
+        as="h1" />
+      <TypographyProse
+        v-if="t('data.translations[0].content')"
+        :content="t('data.translations[0].content')"
+        size="lg"
+        class="py-6 font-display" />
+      <BlocksButtonGroup
+        v-if="data.button_group"
+        :data="data.button_group as BlockButtonGroup" />
+    </div>
+    <!-- Image -->
+    <div
+      v-if="data.image"
+      class="overflow-hidden border lg:relative lg:h-full dark:border-gray-700 rounded-card"
+      :class="
+        data.image_position === 'left'
+          ? 'order-first lg:-ml-48 md:-ml-16'
+          : 'lg:-mr-48 md:-mr-16 '
+      ">
+      <NuxtImg
+        v-if="data.image"
+        class="w-full overflow-hidden dark:brightness-90 max-h-[700px] h-full object-cover rounded-card"
+        :src="data.image as string"
+        alt="" />
+    </div>
+  </BlockContainer>
 </template>
--- types/blocks/block-hero.ts
+++ types/blocks/block-hero.ts
@@ -1,12 +1,13 @@
-import type { File } from '../system';
-import type { BlockButtonGroup } from '../blocks';
+import type { File } from "../system";
+import type { BlockButtonGroup } from "../blocks";
 
 export interface BlockHero {
-	id?: string;
-	title?: string | null;
-	headline?: string | null;
-	content?: string | null;
-	image?: (string | File) | null;
-	image_position?: 'left' | 'right' | null;
-	button_group?: (string | BlockButtonGroup) | null;
+  id?: string;
+  title?: string | null;
+  headline?: string | null;
+  content?: string | null;
+  image?: (string | File) | null;
+  image_position?: "left" | "right" | null;
+  button_group?: (string | BlockButtonGroup) | null;
+  translations?: any | null;
 }
